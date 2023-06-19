fda465bba9c85bf6880d4f2ef4d6d453d7ac28ee
Ridhanya S <82317379+Ridhanya-S@users.noreply.github.com>
2023-06-19 22:52:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,8 @@ load_dotenv('config.env')
 file_path = os.getenv('FILE_PATH')
 output_path = os.getenv('OUTPUT_PATH')
 
-# v_link = "https://www.youtube.com/watch?v=KxvPR0cd3-k" #cc
-# v_link = "https://www.youtube.com/watch?v=rrRuBrUrnCw"
-v_link = "https://www.youtube.com/watch?v=8Yh2pL38ysM"
-
-
 def main():
-  # v_link = input("\nEnter the video link: ")
+  v_link = input("\nEnter the video link: ")
 
   valid=validators.url(v_link)
 
